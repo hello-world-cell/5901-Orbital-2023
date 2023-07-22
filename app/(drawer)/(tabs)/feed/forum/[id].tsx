1a61e9feeb89ea1857a9a4d99e72d23a0dad4141
clarwen <claricelim08@gmail.com>
2023-07-22 18:11:57 +0800

    create comments

--- app/(drawer)/(tabs)/feed/forum/[id].tsx
+++ app/(drawer)/(tabs)/feed/forum/[id].tsx
@@ -8,15 +8,23 @@ import { getPost } from '../../../../../src/graphql/queries';
 export default function TweetScreen() {
     const {id} = useSearchParams();
     const [post, setPost] = useState(null);
+    const [comments, setComments] = useState([]);
+    const [newComment, setNewComment] = useState('');
 
     useEffect(() => {
       const fetchPost = async () => {
         try {
+          //fetch post details
           const postData = await API.graphql(
             graphqlOperation(getPost, { id })
           );
           const fetchedPost = postData.data.getPost;
           setPost(fetchedPost);
+
+          // Fetch the comments associated with the post
+          //const commentsData = await API.graphql(graphqlOperation(listCommentsByPost, { postID: id }));
+          //setComments(commentsData.data.listCommentsByPost.items);
+
         } catch (error) {
           console.log('Error fetching post:', error);
         }
